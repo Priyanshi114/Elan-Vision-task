a301a7ce5a65f458818afea1d7367c5b9f6b3f78
Parul <priyanshiarya@gmail.com>
2026-05-24 21:24:36 +0530

    add some chages in file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const port = 8080;
 const mongoose = require("mongoose"); 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/elan&vision";
+const MONGO_URL = process.env.MONGO_URL;
+await mongoose.connect(MONGO_URL);
+
 
 main().then(() => {
     console.log("connected to db");
@@ -37,44 +38,4 @@ app.post("/register", (req, res) => {
 
 app.listen(port, () => {
     console.log("server is listening");
-=======
-const express = require("express");
-const app = express();
-const port = 8080;
-const mongoose = require("mongoose"); 
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/elan&vision";
-
-main().then(() => {
-    console.log("connected to db");
-}).catch((err) => {
-    console.log(err);
-});
-
-async function main() {
-    await mongoose.connect(MONGO_URL); 
-};
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-   res.render("index");
-});
-
-app.get("/register", (req, res) => {
-  res.render("register"); 
-});
-
-app.post("/register", (req, res) => {
-    let {name, email, year, number, department} = req.body;
-    console.log("New Registration:", name, email, year, number, department);
-    res.render("success", { name });
-});
-
-app.listen(port, () => {
-    console.log("server is listening");
->>>>>>> 7e22b6ccd81638b1f2d6882814a88b5762b1b73f
 });
